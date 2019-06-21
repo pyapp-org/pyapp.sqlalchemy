3330b330bea5ea77aa1bb56276378d6a6ef52954
Tim Savage <tim@savage.company>
2019-06-22 00:19:45 +1000

    Updated to work with latest extension design

--- pyapp_ext/sqlalchemy/__init__.py
+++ pyapp_ext/sqlalchemy/__init__.py
@@ -13,10 +13,3 @@ class Extension:
     """
     default_settings = ".default_settings"
     checks = ".checks"
-
-    @staticmethod
-    def ready():
-        from pyapp.injection import register_factory
-        register_factory(Session, get_session)
-        register_factory(Connection, get_connection())
-        register_factory(Engine, get_engine)
